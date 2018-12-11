fffb234a4cc195f6e35fba0ec2d786c16b339aa6
cthwaite <thwaite@mac.com>
2018-12-11 22:24:44 +0000

    Move FramebufferState into swapchain module

--- src/swapchain.rs
+++ src/swapchain.rs
@@ -52,3 +52,77 @@ impl<B: gfx_hal::Backend> SwapchainState<B> {
         self.back_buffer.take();
     }
 }
+
+
+pub struct FramebufferState<B: gfx_hal::Backend> {
+    framebuffers: Option<Vec<B::Framebuffer>>,
+    image_views: Option<Vec<B::ImageView>>
+}
+
+impl<B: gfx_hal::Backend> FramebufferState<B> {
+    pub fn new(context: &Context<B>, render_pass: &B::RenderPass, swap_state: &mut SwapchainState<B>) -> Self {
+        let mut fbs = FramebufferState::new_empty();
+        fbs.rebuild_from_swapchain(context, render_pass, swap_state);
+        fbs
+    }
+
+    pub fn new_empty() -> Self {
+        FramebufferState {
+            framebuffers: None,
+            image_views: None,
+        }
+    }
+
+    pub fn rebuild_from_swapchain(&mut self, context: &Context<B>, render_pass: &B::RenderPass, swap_state: &mut SwapchainState<B>) {
+        let (image_views, framebuffers) = match swap_state.back_buffer.take().unwrap() {
+            Backbuffer::Images(images) => {
+                let color_range = SubresourceRange {
+                    aspects: Aspects::COLOR,
+                    levels: 0..1,
+                    layers: 0..1,
+                };
+
+                let image_views = context.map_to_image_views(
+                    &images,
+                    ViewKind::D2,
+                    Swizzle::NO,
+                    color_range,
+                ).unwrap();
+                let fbos = context.image_views_to_fbos(&image_views, &render_pass, swap_state.extent.clone()).unwrap();
+
+                (image_views, fbos)
+            },
+            Backbuffer::Framebuffer(fbo) => (Vec::new(), vec![fbo]),
+        };
+        self.framebuffers = Some(framebuffers);
+        self.image_views = Some(image_views);
+    }
+
+    pub fn is_some(&self) -> bool {
+        self.framebuffers.is_some() && self.image_views.is_some()
+    }
+
+    pub fn is_none(&self) -> bool {
+        self.framebuffers.is_none() || self.image_views.is_none()
+    }
+
+    pub fn get_mut(&mut self) -> (&mut Vec<B::ImageView>, &mut Vec<B::Framebuffer>) {
+        (
+            self.image_views.as_mut().unwrap(),
+            self.framebuffers.as_mut().unwrap(),
+        )
+    }
+
+    pub fn destroy(&mut self, device: &B::Device) {
+        if let Some(framebuffers) = self.framebuffers.take() {
+            for framebuffer in framebuffers {
+                device.destroy_framebuffer(framebuffer);
+            }
+        }
+        if let Some(image_views) = self.image_views.take() {
+            for image_view in image_views {
+                device.destroy_image_view(image_view);
+            }
+        }
+    }
+}
