e2783497c4dc9a1fd485785c6d2bf4a48d3f0671
cthwaite <thwaite@mac.com>
2018-12-15 15:26:38 +0000

    rustfmt pass on buffer; unwrap release_mapping_writer in fill

--- src/buffer.rs
+++ src/buffer.rs
@@ -148,7 +148,7 @@ impl<B: gfx_hal::Backend> Buffer<B> {
         // TODO: return result
         let mut dest = device.acquire_mapping_writer::<T>(memory, 0..buffer_len)?;
         dest.copy_from_slice(data);
-        device.release_mapping_writer(dest);
+        device.release_mapping_writer(dest).unwrap();
         Ok(())
     }
 
@@ -163,26 +163,27 @@ impl<B: gfx_hal::Backend> Buffer<B> {
     }
 }
 
-
 pub fn empty_buffer<B: Backend, Item>(
-        device: &B::Device,
-        memory_types: &[MemoryType],
-        properties: Properties,
-        usage: buffer::Usage,
-        item_count: usize) -> (B::Buffer, B::Memory) {
+    device: &B::Device,
+    memory_types: &[MemoryType],
+    properties: Properties,
+    usage: buffer::Usage,
+    item_count: usize,
+) -> (B::Buffer, B::Memory) {
     let stride = ::std::mem::size_of::<Item>() as u64;
     let buffer_len = item_count as u64 * stride;
     let unbound_buffer = device.create_buffer(buffer_len, usage).unwrap();
     let req = device.get_buffer_requirements(&unbound_buffer);
 
-    let upload_type = memory_types.iter()
-                                  .enumerate()
-                                  .find(|(id, ty)| {
-                                      let type_supported = req.type_mask & (1_u64 << id) != 0;
-                                      type_supported && ty.properties.contains(properties)
-                                  })
-                                  .map(|(id, _ty)| MemoryTypeId(id))
-                                  .expect("Could not find appropriate vertex buffer memory type.");
+    let upload_type = memory_types
+        .iter()
+        .enumerate()
+        .find(|(id, ty)| {
+            let type_supported = req.type_mask & (1_u64 << id) != 0;
+            type_supported && ty.properties.contains(properties)
+        })
+        .map(|(id, _ty)| MemoryTypeId(id))
+        .expect("Could not find appropriate vertex buffer memory type.");
     let buffer_memory = device.allocate_memory(upload_type, req.size).unwrap();
     let buffer = device
         .bind_buffer_memory(&buffer_memory, 0, unbound_buffer)
@@ -191,23 +192,28 @@ pub fn empty_buffer<B: Backend, Item>(
     (buffer, buffer_memory)
 }
 
-pub fn fill_buffer<B: gfx_hal::Backend, T: Copy>(device: &B::Device, buffer_memory: &B::Memory, data: &[T]) {
+pub fn fill_buffer<B: gfx_hal::Backend, T: Copy>(
+    device: &B::Device,
+    buffer_memory: &B::Memory,
+    data: &[T],
+) {
     let stride = ::std::mem::size_of::<T>() as u64;
     let buffer_len = data.len() as u64 * stride;
 
-    let mut dest = device.acquire_mapping_writer::<T>(&buffer_memory, 0..buffer_len)
-                        .unwrap();
+    let mut dest = device
+        .acquire_mapping_writer::<T>(&buffer_memory, 0..buffer_len)
+        .unwrap();
     dest.copy_from_slice(data);
     device.release_mapping_writer(dest);
 }
 
-
 pub fn create_buffer<B: Backend, Item: Copy>(
-        device: &B::Device,
-        memory_types: &[MemoryType],
-        properties: Properties,
-        usage: buffer::Usage,
-        items: &[Item]) -> (B::Buffer, B::Memory) {
+    device: &B::Device,
+    memory_types: &[MemoryType],
+    properties: Properties,
+    usage: buffer::Usage,
+    items: &[Item],
+) -> (B::Buffer, B::Memory) {
     let (empty_buffer, mut empty_buffer_memory) =
         empty_buffer::<B, Item>(device, memory_types, properties, usage, items.len());
 
